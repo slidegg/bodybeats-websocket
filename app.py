d971f892bc783ec31dbab28d6661c8d2774665aa
Greg Papaspyropoulos <greg.papaspiropoulos@gmail.com>
2022-09-08 19:36:44 +0200

    add agents 2

--- app.py
+++ app.py
@@ -7,6 +7,8 @@ import websockets
 import json
 
 
+
+# REFFERENCE
 async def echo(websocket):
     async for message in websocket:
         await websocket.send(message)
@@ -17,6 +19,53 @@ async def hello(websocket):
     greeting = f"Hello {name}!"
     await websocket.send(greeting)
     await echo(websocket)
+
+# END OF REFERENCE
+
+async def error(websocket, message):
+    """
+    Send an error message.
+
+    """
+    event = {
+        "type": "error",
+        "message": message,
+    }
+    await websocket.send(json.dumps(event))
+
+
+async def play(websocket, game, player, connected):
+    """
+    Receive and process moves from a player.
+
+    """
+    async for message in websocket:
+        # Parse a "play" event from the UI.
+        event = json.loads(message)
+        assert event["type"] == "play"
+        column = event["column"]
+
+        # Send a "play" event to update the UI.
+        event = {
+            "type": "play",
+            "player": player,
+            "column": column,
+            "row": row,
+        }
+        websockets.broadcast(connected, json.dumps(event))
+
+        # If move is winning, send a "win" event.
+        if game.winner is not None:
+            event = {
+                "type": "win",
+                "player": game.winner,
+            }
+            websockets.broadcast(connected, json.dumps(event))
+
+
+
+
+
 
 async def main():
     # Set the stop condition when receiving SIGTERM.
